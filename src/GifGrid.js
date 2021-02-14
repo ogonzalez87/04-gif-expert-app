b468248658686af8f95c980e49720bcef22bfaf3
Oscar <ofgonzalez87@gmail.com>
2021-02-14 19:55:50 -0400

    docs agregado

--- src/GifGrid.js
+++ src/GifGrid.js
@@ -1,29 +1,21 @@
-import React from 'react';
-import { useFetchGifs } from '../src/hooks/useFetchGifs';
-import { GifGridItem } from '../src/components/GifGridItem';
+import React from "react";
+import { useFetchGifs } from "../src/hooks/useFetchGifs";
+import { GifGridItem } from "../src/components/GifGridItem";
 
 export const GifGrid = ({ category }) => {
+  const { data: images, loading } = useFetchGifs(category);
 
-    const { data:images, loading } = useFetchGifs( category );
+  return (
+    <>
+      <h3 className="animate__animated animate__fadeIn"> {category} </h3>
 
-    return (
-        <>
-            <h3 className="animate__animated animate__fadeIn"> { category } </h3>
+      {loading && <p className="animate__animated animate__flash">Loading</p>}
 
-            { loading && <p className="animate__animated animate__flash">Loading</p> }
-
-            <div className="card-grid">
-                
-                {
-                    images.map( img => (
-                        <GifGridItem 
-                            key={ img.id }
-                            { ...img }
-                        />
-                    ))
-                }
-            
-            </div>
-        </>
-    )
-}
+      <div className="card-grid">
+        {images.map((img) => (
+          <GifGridItem key={img.id} {...img} />
+        ))}
+      </div>
+    </>
+  );
+};
