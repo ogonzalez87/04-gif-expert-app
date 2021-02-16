2834d0f18d0b4a4249fc2b6091093d90696a459b
Oscar <ofgonzalez87@gmail.com>
2021-02-15 21:10:13 -0400

    commit 90

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,13 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+ 
+  
 
 export const GifGridItem = ({ title, url }) => {
- 
   return (
-    <div className = "card animate__animated animate__bounce" >
-      
+    <div className="card animate__animated animate__bounce">
       <img src={url} alt={title} />
 
       <p> {title} </p>
     </div>
   );
+};
+
+GifGridItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 };
--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,12 +3,34 @@ import { shallow } from "enzyme";
 import { GifGridItem } from "../../components/GifGridItem";
 
 describe("<GifGridItem/>", () => {
+  const title = "Un titulo";
+  const url = "google.com";
+  const wrapper = shallow(<GifGridItem title={title} url={url} />);
+
   test("should load commponent", () => {
-    const wrapper = shallow(<GifGridItem />);
-
-
-    
-
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test("should work p", () => {
+    const p = wrapper.find("p");
+
+    expect(p.text().trim()).toBe(title);
+  });
+  test("should work img", () => {
+    const img = wrapper.find("img");
+
+    expect(img.prop("src")).toBe(url);
+    expect(img.prop("alt")).toBe(title);
+  });
+
+  test("should have fadein", () => {
+    const div = wrapper.find("div");
+
+    // console.log( div.html());
+    console.log(div.prop("className"));
+
+    const className = div.prop("className");
+
+    expect(className.includes("animate__bounce")).toBe(true);
   });
 });
