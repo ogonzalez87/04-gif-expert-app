755440501b4354fa382e3a5c7e024c5a76bcb8e9
Oscar <ofgonzalez87@gmail.com>
2021-02-17 12:43:30 -0400

    x

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,13 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export const GifGridItem = ({ title, url }) => {
- 
   return (
-    <div className = "card animate__animated animate__bounce" >
-      
+    <div className="card animate__animated animate__bounce">
       <img src={url} alt={title} />
 
       <p> {title} </p>
     </div>
   );
+};
+GifGridItem.prototypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 };
--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { GifGridItem } from "../../components/GifGridItem";
+const title = "Un título";
+const url = "https://google.com";
+const wrapper = shallow(<GifGridItem title={title} url={url} />);
 
 describe("<GifGridItem/>", () => {
   test("should load commponent", () => {
-    const wrapper = shallow(<GifGridItem />);
-
-
-    
-
     expect(wrapper).toMatchSnapshot();
   });
+
+  test("should have tittle", () => {
+    const p = wrapper.find("p");
+
+    expect(p.text().trim()).toBe(title);
+  });
+
+  test('should have img with src and alt', () => {
+    const img= wrapper.find('img');
+
+    
+  })
+  
 });
