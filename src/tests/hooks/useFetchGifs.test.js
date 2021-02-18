c0e19009da64a88a0cf667716961510e7ccac2bc
Oscar <ofgonzalez87@gmail.com>
2021-02-18 14:11:44 -0400

    Commit 100 fin S8

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -4,15 +4,27 @@ import { useFetchGifs } from "../../hooks/useFetchGifs";
 import { renderHook } from "@testing-library/react-hooks";
 
 describe("Test on hook useFetchGifs", () => {
-  test("should return initial state", () => {
-    const { result } = renderHook(() => useFetchGifs("One Punch"));
+  test("should return initial state", async() => {
+    const { result ,waitForNextUpdate} = renderHook(() => useFetchGifs("One Punch"));
 
     const { data, loading } = result.current;
+
+    await waitForNextUpdate();
     console.log(data, loading);
 
     expect(data).toEqual([]);
     expect(loading).toBe(true);
   });
+
+  test('should should return img array', async() => {
+    const { result,waitForNextUpdate } = renderHook(() => useFetchGifs("One Punch"));
+    await waitForNextUpdate();
+    const { data, loading } = result.current;
+    
+    expect(data.length).toBe(10);
+    expect(loading).toBe(false);
+  })
+  
 });
 
 //npm install --save-dev @testing-library/react-hooks
